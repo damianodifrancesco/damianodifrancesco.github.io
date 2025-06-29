78b29376bd23fdf450c6c903cfc939b6f23dfa76
damianodifrancesco <87653881+damianodifrancesco@users.noreply.github.com>
2025-06-29 20:28:08 +0000

    Deploying to gh-pages from @ damianodifrancesco/damianodifrancesco.github.io@dbd13580ddcea67d5a84b07b81809010bd42a4d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "You can download my cv here.",
           section: "Navigation",
           handler: () => {
