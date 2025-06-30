5cc898df0a955c706b77aea94d4baae0a1e57440
damianodifrancesco <87653881+damianodifrancesco@users.noreply.github.com>
2025-06-30 09:29:55 +0000

    Deploying to gh-pages from @ damianodifrancesco/damianodifrancesco.github.io@343015d6bdb1698249ea85268842170ff02f6e7e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,35 +459,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%61%6D%69%61%6E%6F.%64%69%66%72%61%6E%63%65%73%63%6F@%73%61%6E%74%61%6E%6E%61%70%69%73%61.%69%74", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/damiano-di-francesco-191ba91a9/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/https://x.com/Damiano_Difra", "_blank");
         },
       },{
       id: 'light-theme',
